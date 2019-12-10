2d4d98a1080ac42ec019071e305532dc5042d6a2
Warit Santaputra <waritsan@gmail.com>
2019-12-10 17:59:58 +0700

    Finish but not working

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,4 @@
 import '../polyfills';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-
 platformBrowserDynamic().bootstrapModule(AppModule);
